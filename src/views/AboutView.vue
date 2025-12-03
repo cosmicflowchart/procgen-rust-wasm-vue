c7baa4864c17d46430122a0c423d9e829a15414e
Ulrich Feindt <ulrich@feindt.dev>
2025-12-03 21:45:59 +0100

    Initial wasm setup

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+import { sum, divide, multiply, subtract } from 'procgen'
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <h1>This is an about page</h1>
+
+      <p>Let's do some math... with wasm!</p>
+      <p>1 + 2 = {{ sum(1, 2) }}</p>
+      <p>1 - 2 = {{ subtract(1, 2) }}</p>
+      <p>1 * 2 = {{ multiply(1, 2) }}</p>
+      <p>6 / 2 = {{ divide(6, 2) }}</p>
+    </div>
   </div>
 </template>
 
